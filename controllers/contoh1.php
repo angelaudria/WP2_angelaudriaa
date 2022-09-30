20fe6f258d0f5c11bc4d49c0276abf42c12a34ec
angelaudria <113443354+angelaudria@users.noreply.github.com>
2022-10-01 00:02:57 +0700

    Add files via upload

---
+++ controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Angel Audria<br>
+            Saya tinggal di Bekasi<br>
+            Saya kuliah di universitas<br> 
+            Bina Sarana Informatika";
+    }
+}
