71013b1bd4bd50c96b30bd3e2cf3d9558ab451fa
angelaudria <113443354+angelaudria@users.noreply.github.com>
2022-10-01 00:10:06 +0700

    Add files via upload

---
+++ application/views/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+
+</head>
+<body>
+    Halo kawan.. Yuk kita belajar WEB Programming!<br>
+    <br>Nilai1 = <?= $nilai1; ?></br>
+    <br>Nilai2 = <?= $nilai2; ?></br>
+    <br>Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?></br>
+
+</body>
+</html>
